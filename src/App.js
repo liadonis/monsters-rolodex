e74ab0a3d8d922b521b3070fb191ed3122fffae0
adonis <li.adonis@gmail.com>
2021-11-17 10:42:03 +0800

    L36

--- src/App.js
+++ src/App.js
@@ -13,16 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList>
-          {
-            this.state.monsters.map((monster, index) => {
-              return (
-                <h1 key={monster.id}>{monster.name}</h1>
-              )
-            })
-          }
-        </CardList>
-
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.style.css';
 
-export const CardList = props => {
-    return (
-    <div className='card-list'>{props.children}</div>
-    )
-}
+export const CardList = props => (
+    <div className='card-list'>
+        {
+            props.monsters.map((monster, index) => {
+                return (
+                    <Card key={monster.id} monster={monster} />
+
+                )
+            })
+        }
+    </div>
+)
