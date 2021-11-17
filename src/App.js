84eef156fc917ec07656cee065faf0ec4cc0ab44
adonis <li.adonis@gmail.com>
2021-11-17 11:29:39 +0800

    L41

--- src/App.js
+++ src/App.js
@@ -12,18 +12,22 @@ class App extends Component {
   }
 
   render() {
-    
+    const { monsters , searchField } = this.state;
+
+    // var filteredNum = numbers.filter(function(value) {
+    //   return value > 10;
+    // });
+
+    //const searchFilter = monsters.filter(f => f.indexof(searchField) != -1);
+    const filteredMonters = monsters.filter( monster => 
+       monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <input type='search' placeholder='search monsters'
-          onChange={e =>
-            this.setState(
-              { searchField: e.target.value },
-              () => console.log(this.state.searchField)
-            )
-          }
+          onChange={ e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters}></CardList>
+        <CardList monsters={filteredMonters}></CardList>
       </div>
     );
   }
